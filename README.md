b9e0e0128f8826f1f7ee91355547ec56b453e464
cottayson <cottayson@gmail.com>
2021-02-10 23:14:21 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# What it is?
+Is a dark theme for <observablehq.com>
+
+# How to use
+1. Install [Violentmonkey](https://violentmonkey.github.io/) extension
+1. Load `test.user.js` script.
+1. Open your favorite observable [notebook](https://observablehq.com/@observablehq/input-checkbox)
+
+# Contributing
+
+## Setting up your favourite editor
+* Firstly read this [Install a local script](https://violentmonkey.github.io/posts/how-to-edit-scripts-with-your-favorite-editor/#install-a-local-script)
+
+## Let's use "hard way"
+- Your browser is `Google Chrome` or `Mozilla Firefox`
+- [Install](https://nodejs.org/en/download/) `node.js` if it's not installed
+- Run `npm install` in project directory
+- Start http server by running `npm start` or `http-server -c5`
+- Open url in browser <http://localhost:8080/test.user.js>
+- Click on `options` button and select `Track local file before this window is closed`
+- Click `Confirm installation` or `install` button
+
+Note: **You will need to reload browser after each script change.**
+
+### Issues:
+* [ ] User don't see cursor
+* [ ] User don't see selected text
+* [ ] User don't see current working cell
+* [ ] Booleans are dark green, it's bad
+* [ ] Code completion (you can press **TAB** for code completion) use wrong background and text colors
+* [ ] Buttons! Publish, like, menu
+* [ ] Right panel \`graph of dependencies\` needs more contrast colors
+* [ ] Help ("?" in bottom right corner)
+  + [ ] Keyboard shortcuts 
+  + [ ] Other submenus
+* [ ] Notebook comments have white background
---
+++ test.user.js
@@ -0,0 +1,181 @@
+// ==UserScript==
+// @name        Dark Theme for observablehq.com
+// @namespace   Violentmonkey Scripts
+// @match       *://*/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 10.02.2021, 15:20:11
+// @inject-into auto
+// ==/UserScript==
+
+var style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = `
+:root {
+  /* background colors */
+  --bg-color: #353535;
+  --pea-bg-color: #303030;
+  --pea-bg-hover-color: #333333;
+  --items-bg-color: #353535;
+  --button-bg-color: #353535;
+
+  /* text colors */
+  --text-color: #eee;
+  --text-color-2: #ccc;
+  --min-text-color: #eee;
+  --link-text-color: #ccf;
+
+  /* other */
+  --global-border-color: #444;
+
+  /* code highlighting colors */
+  --code-string: #ccf;
+  --code-collapsed-text: #ccc;
+  --code-unknown-variable: #ccc;
+  --code-error-color: #f22;
+  --code-number: #0f0;
+  --code-regexp: #ff5;
+}
+
+.site-header {
+  background-color: var(--bg-color);
+}
+
+.observable-navy {
+  color: var(--text-color);
+}
+
+h1, h2, h3, h4, h5 {
+  color: var(--text-color);
+}
+
+p, .observablehq {
+  color: var(--text-color);
+}
+
+body, div.observablehq {
+  background-color: var(--bg-color);
+  color: var(--min-text-color);
+}
+
+.button-normal {
+  background-color: var(--button-bg-color);
+}
+
+.pea {
+  background-color: var(--pea-bg-color);
+}
+
+.pea:hover {
+  background-color: var(--pea-bg-hover-color);
+}
+
+div.flex.items-center, div.flex.bg-white, div.pa2 {
+  background-color: var(--items-bg-color);
+  color: var(--min-text-color);
+}
+
+footer.bg-near-white {
+  background-color: var(--bg-color);
+}
+
+.dark-gray {
+  color: var(--text-color-2);
+}
+
+.b--light-silver {
+  border-color: var(--global-border-color);
+}
+
+.bg-white, .bg-near-white {
+  background-color: var(--bg-color);
+}
+
+/* ----CODE STYLE BEGIN---- */
+
+.observablehq--collapsed, .observablehq--expanded, .observablehq--function, .observablehq--gray, .observablehq--import, .observablehq--string:after, .observablehq--string:before {
+  color: var(--code-collapsed-text);
+}
+
+.cm-s-observable span.cm-string {
+  color: var(--code-string);
+}
+
+.cm-s-observable span.cm-string-2 {
+  color: var(--code-string);
+}
+
+.cm-s-observable span.cm-property {
+  color: #e8e8e8;
+}
+
+.observablehq--blue, .observablehq--keyword, a[href]
+{
+  color: var(--link-text-color);
+}
+
+.observablehq--index, .observablehq--key {
+  color: #88f; /*88f key in array*/
+}
+
+.cm-s-observable span.cm-atom {
+  color: #aaf; /* boolean true, false */
+}
+
+.cm-s-observable span.cm-def {
+  color: var(--text-color); /* variable */
+}
+
+.cm-s-observable span.cm-variable {
+  color: var(--code-unknown-variable);
+}
+
+
+.cm-s-observable span.cm-variable-2 {
+  color: #aff;
+}
+
+.cm-s-observable span.cm-comment {
+  color: #bbb;
+}
+
+.cm-s-observable span.cm-keyword {
+  color: #f88; /* for keyword */
+}
+
+.CodeMirror, CodeMirror-wrap pre.CodeMirror-line {
+  color: #f00; /* brackets color */
+}
+
+.cm-s-observable.CodeMirror {
+  color: #fff; /* brackets color */
+}
+
+.cm-s-observable.CodeMirror .CodeMirror-matchingbracket {
+  color: #fff !important;
+}
+
+div.observablehq--inspect {
+  color: var(--code-error-color);
+}
+
+.cm-s-observable span.cm-number, .cm-s-observable {
+  color: var(--code-number);
+}
+
+.cm-s-observable span.cm-regexp, .observablehq--regexp {
+  color: var(--code-regexp);
+}
+
+.observablehq--number {
+  color: var(--code-number);
+}
+
+.observablehq--string {
+  color: var(--code-string);
+}
+
+/* ----CODE STYLE END---- */
+`;
+document.getElementsByTagName('head')[0].appendChild(style);
